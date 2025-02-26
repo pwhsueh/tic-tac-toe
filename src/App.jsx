80b15989238ef01258cd6d2ac14455118eddd19c
bowen_hsueh <pwhsueh@gmail.com>
2025-02-26 09:09:28 +0800

    initialize React Tic-Tac-Toe project with Vite, create main components and configuration files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Player from './components/Player.jsx';
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player1" symbol="X" />
+          <Player name="Player2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
+}
+
+export default App
